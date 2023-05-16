53b2be3b3642a68fb116fa95c7170d478ca38ddc
Ontiimeyes <lucassalinroge@gmail.com>
2023-05-16 16:21:45 +0200

    Setting up OAuth frontend

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -6,7 +6,7 @@ function	Login()
 {
 	const	redirectToIntra = () =>
 	{
-		const	intraAuthUrl = "https://intra.42.fr/oauth/authorize?client_id=u-s4t2ud-d73ae5d41fb1e973ad2df622868f6641c621b3c78fb8a03dc5f70cb70ab23cf4D&redirect_uri=https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-d73ae5d41fb1e973ad2df622868f6641c621b3c78fb8a03dc5f70cb70ab23cf4&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcallback&response_type=code&response_type=code";
+		const intraAuthUrl = "https://intra.42.fr/oauth/authorize?client_id=u-s4t2ud-d73ae5d41fb1e973ad2df622868f6641c621b3c78fb8a03dc5f70cb70ab23cf4&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcallback&response_type=code";
 		window.location.href = intraAuthUrl;
 	};
 
